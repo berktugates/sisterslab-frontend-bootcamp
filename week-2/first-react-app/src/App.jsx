0a3fc220c9030d1cd91bc216fd03a4313b2b0668
Berktug Berke Ates <berktugates@gmail.com>
2023-10-21 00:31:56 +0300

    fixed props structure

--- week-2/first-react-app/src/App.jsx
+++ week-2/first-react-app/src/App.jsx
@@ -1,10 +1,19 @@
 import Header from "./components/Header"
 import CardComponent from "./components/cardComponent";
 function App() {
+  const customer = {
+    name : "Berktug",
+    surname : "Ates",
+    age : 22
+  }
   return(
     <div>
       <Header/>
-      <CardComponent/>
+      <CardComponent
+        name = {customer.name}
+        surname = {customer.surname}
+        age = {customer.age}
+      />
     </div>
    
   )
--- week-2/first-react-app/src/components/CardComponent.jsx
+++ week-2/first-react-app/src/components/CardComponent.jsx
@@ -1,17 +1,11 @@
-const CardComponent = () => {
-    const customer = 
-        {
-            name : "Berktug",
-            surname : "Ates",
-            age : 22
-        }
-    ;
+const CardComponent = (props) => {
+    const {name,surname,age} = props
   return (
     <>
         <div className='bg-gray-200 w-1/4 rounded-lg mt-3 ms-3 p-3 border shadow-lg'>
-            <h1>Name : {customer.name}</h1>
-            <h2>Surname : {customer.surname}</h2>
-            <p>Age : {customer.age}</p>
+            <h1>Name : {name}</h1>
+            <h2>Surname : {surname}</h2>
+            <p>Age : {age}</p>
         </div>
     </>
   )
